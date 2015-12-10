47a57fcb9f9be0fca36b1976da12e493eab12248
Abubacker Siddik A <abuabdul86@hotmail.com>
2015-12-10 23:51:25 +0530

    Logger added

--- src/main/java/com/abuabdul/dobiztext/controller/DoBizTextOverviewController.java
+++ src/main/java/com/abuabdul/dobiztext/controller/DoBizTextOverviewController.java
@@ -1,5 +1,7 @@
 package com.abuabdul.dobiztext.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,8 +14,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class DoBizTextOverviewController {
 
+	private static final Logger log = LogManager.getLogger(DoBizTextOverviewController.class.getName());
+
 	@RequestMapping("/landing/doBizTextOverview.go")
 	public String overviewPage() {
+		log.debug("Entering overviewPage() in the DoBizTextOverviewController");
 		return "overview";
 	}
 
